6d8373d3fbc5f914533ed432eba7ae18b765c8e1
wmyers <ec23consulting@gmail.com>
2015-01-21 19:22:31 +0800

    adding chat app, preliminary css and static template

--- client/app/dash/dash.config.js
+++ client/app/dash/dash.config.js
@@ -8,7 +8,8 @@ angular.module('dashMdul')
           {route:'dashboard.modalDemo', title:'Modal Demo'},
           {route:'dashboard.accordionDemo', title:'Accordion Demo'},
           {route:'dashboard.responsiveTableDemo', title:'Responsive Table Demo'},
-          {route:'dashboard.responsiveTabsDemo', title:'Responsive Tabs Demo'}
+          {route:'dashboard.responsiveTabsDemo', title:'Responsive Tabs Demo'},
+          {route:'dashboard.chat', title:'Chat'}
         ],
     showSideBarAtStart:false,
     brand:'Component Demos'
---
+++ client/components/chat/chat.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('dashMdul')
+.config(function($stateProvider){
+  $stateProvider
+  .state('dashboard.chat', {
+    url: '/chat-demo',
+    templateUrl: './components/chat/chat.html',
+    controller: 'chatCtrl'
+  });
+})
+//modal demo ctrl
+.controller('chatCtrl',
+[
+  '$scope',
+  function (
+    $scope
+  ) {
+
+
+    }]);
