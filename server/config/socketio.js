dd8976a2bdb8439a2a40a26fc0b8b35bfde762bb
wmyers <ec23consulting@gmail.com>
2015-01-26 16:59:01 +0800

    socketio wip

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -1,11 +1,3 @@
 module.exports = function(io){
   io.set('log level', 1);
-
-  //TODO move this somewhere else
-  io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
-    socket.on('send', function (data) {
-      io.sockets.emit('message', data);
-    });
-  });
 };
---
+++ server/socketio/index.js
@@ -0,0 +1,60 @@
+module.exports = function(io){
+  // io.sockets.on('connection', function (socket) {
+  //   socket.emit('message', { message: 'welcome to the chat' });
+  //   socket.on('send', function (data) {
+  //     io.sockets.emit('message', data);
+  //   });
+  // });
+
+
+  //auto create bigmouth namespace
+  var bigmouth = io.of('/bigmouth').on('connection', function (socket) {
+
+    //helper
+    var getRoomClients = function(roomId) {
+      return Object.keys(io.nsps['/bigmouth'].adapter.rooms[roomId]);
+    }
+
+    socket.on('joinRoomRequest',function(data){
+      var username = data.user;
+      var roomId = data.roomId;
+
+      // var avatarUrl = gravatar.url(data.avatar, {s: '140', r: 'x', d: 'mm'});
+      // socket.emit('img', socket.avatar);
+
+      socket.join(roomId);
+    });
+
+      // var usernames = [],
+      // avatars = [];
+      // usernames.push(chat.clients(data.id)[0].username);
+      // usernames.push(chat.clients(data.id)[1].username);
+      // avatars.push(chat.clients(data.id)[0].avatar);
+      // avatars.push(chat.clients(data.id)[1].avatar);
+      // chat.in(data.id).emit('startChat', {
+      //   boolean: true,
+      //   id: data.id,
+      //   users: usernames,
+      //   avatars: avatars
+      // });
+
+
+    socket.on('disconnect', function() {
+
+      socket.broadcast.to(socket.room).emit('leave', {
+        boolean: true,
+        room: this.room,
+        user: this.username,
+        avatar: this.avatar
+      });
+
+      socket.leave(socket.room);
+    });
+
+
+    socket.on('msg', function(data){
+
+      socket.broadcast.to(socket.room).emit('receive', {msg: data.msg, user: data.user, img: data.img});
+    });
+  });
+};
